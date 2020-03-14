bd76f49021829a5cf7c0e89ce81e5de71be231cd
nirbhayjain20 <54281726+nirbhayjain20@users.noreply.github.com>
2020-03-14 20:04:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
-  secret: 'secret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }))
